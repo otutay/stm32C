04a490150df9cdec121c711bbda877128cac8dbd
otutay <otutaysalgir@gmail.com>
2021-01-03 00:31:48 +0300

    oc eclipse

--- Inc/Clock.h
+++ Inc/Clock.h
@@ -128,11 +128,57 @@ typedef enum {
 		div512 = 0xF
 } tAHBPrescalerEnum;
 
-void setHSEClock();
-void setPllParam();
-void setSystemClock();
-void GPIOClkReset(int gpioNum);
-void GPIOClkEn(int gpioNum);
-void setAhbFreq(tAHBPrescalerEnum prescaler);
+
+/// functions
+
+void setHSEClock(){
+	pRccCR -> hsiOn  = 0;
+	pRccCR -> hseByp = 0;
+	pRccCR -> hseOn  = 1;
+	while(!(pRccCR-> hseRdy));
+}
+
+
+void setPllParam(){
+	// HSE is used and 168 Mhz is set
+	// vco is around 336
+	// pllN/pllM = 42
+
+	pRccCfg -> pllP = 0; // 0 means 2 -> ref pg 168
+	pRccCfg -> pllM = 4;
+	pRccCfg -> pllN = 168;
+	// calculations are as follows
+	// 8*168/4 = vcoOut -> vcoOut/2 = frequency = 168;
+
+	// set usb freq as 48 Mhz
+	pRccCfg -> pllQ = 7;
+
+	pRccCfg-> pllSrc = 1; // HSE is selected as pllSource;
+
+}
+
+void setSystemClock(){
+	pRccCR-> pllOn = 1;
+	while(!(pRccCR-> pllRdy)); // wait until pll ready
+	pRccClkCfg -> sw = 2; // set pll as system clock
+}
+
+void GPIOClkReset(int gpioNum){
+	// reset
+	pRCCAhb1Rst-> gpioRst = gpioNum;
+	//set
+	pRCCAhb1Rst-> gpioRst = 0;
+}
+
+void GPIOClkEn(int gpioNum){
+	pRccClkEn -> gpioEn = gpioNum;
+}
+
+void setAhbFreq(tAHBPrescalerEnum prescaler){
+	pRccClkCfg-> ahbPres = prescaler;
+}
+
+
+
 
 #endif
